2fce8a3544ae24d0bf1068271ae2e0512bf3040f
Grace Tang <grace.tang1028@gmail.com>
2021-05-17 21:17:39 +1000

    Take in selection as a string, allow upper and lowercase options excluding everything else

--- sourcecode.cpp
+++ sourcecode.cpp
@@ -11,11 +11,11 @@ int voterdata_file()
 	voterdata.open("Voter Data Base.txt");
 	if (!voterdata)
 	{
-		cout << "file not created";
+		cout << "File not created";
 	}
 	else
 	{
-		cout << "file created successfully!";
+		cout << "File created successfully!";
 		voterdata << "Voter Data Base\n";
 		voterdata << setw(15) << "Voter ID" << setw(15) << "Name" << setw(15) << "Age" << setw(15) << "Suburb" << setw(15) << "Status" << endl;
 		voterdata << setw(15) << 12345678 << setw(15) << "Tommy Sauche" << setw(15) << "23" << setw(15) << "Carlton" << setw(15) << "Student" << endl;
@@ -31,11 +31,11 @@ int candidatedata_file()
 	candidatedata.open("candidatedata.txt");
 	if (!candidatedata)
 	{
-		cout << "file not created";
+		cout << "File not created";
 	}
 	else
 	{
-		cout << "file created successfully!";
+		cout << "File created successfully!\n";
 		candidatedata << "Candidate Data Base\n";
 		candidatedata << "Party" << "Name" << "Age" << "District" << "Count" << endl;
 		candidatedata << "Greens" << "Jane Goodie" <<
@@ -57,65 +57,68 @@ int main()
 		}
 		voterdata.close();
 	}
-	else cout << "Unable to open file";
+	else cout << "Unable to open file\n";
 
 
-// Taking in selection 
-	char menu;
-	cout << "Please enter your choice: ";
+// Taking in selection as a string, as the options are in alphabet letters
+	string menu;
+	cout << "Please enter your choice: \n";
 	cout << "Enter P - Print numbers for a particular candidate\n";
 	cout << "Enter A - Add number of votes to a candidate\n";
-	cout << "Enter S - Display the smallest number of votes candidate\n";
-	cout << "Enter L - Display the largest number of votes candidate\n";
+	cout << "Enter S - Display the candidate with the smallest number of votes\n";
+	cout << "Enter L - Display the candidate with the largest number of votes\n";
 	cout << "Enter Q - Quit";
 	cin >> menu;
 
-// Using if-else-if statement to accommodate all cases
-// To-do: make the input case INsesitive
+// Using if-else-if statement to accommodate upper/lowercase options
 
-	if (menu == p) {
+	if (menu == "p" || menu == "P") {
 		voterdata_file();
 		cout << "Writing to the Voter file" << endl;
-		cout << "Please enter your Voter ID: "; cin>> id
-		cin.getline(id, 100);
-		voterdata << id <<"\t";
-		cout << "Please enter your name: "; cin >> name;
-		cin.getline(name, 100);
-		voterdata << name << "\t";
-		cout << "Please enter your age:"; cin >> age;
-		cin.getline(age, 100); cin >> age;
-		voterdata << age << "\t";
-		cin.ignore();
+		// cout << "Please enter your Voter ID: "; cin>> id
+		// cin.getline(id, 100);
+		// voterdata << id <<"\t";
+		// cout << "Please enter your name: "; cin >> name;
+		// cin.getline(name, 100);
+		// voterdata << name << "\t";
+		// cout << "Please enter your age:"; cin >> age;
+		// cin.getline(age, 100); cin >> age;
+		// voterdata << age << "\t";
+		// cin.ignore();
 
 		
 		
 	}
-	else if (menu == a) {
+	else if (menu == "a" || menu == "A") {
 		candidatedata_file();
 		cout << "Writing to the Candidate file" << endl;
-		cout << "Please enter your Party: "; cin >> party
-			cin.getline(party, 100);
-		voterdata << id << "\t";
-		cout << "Please enter your name: "; cin >> name;
-		cin.getline(name, 100);
-		voterdata << name << "\t";
-		cout << "Please enter your age:"; cin >> age;
-		cin.getline(age, 100); cin >> age;
-		voterdata << age << "\t";
-		cin.ignore();
+		// cout << "Please enter your Party: "; cin >> party
+		// 	cin.getline(party, 100);
+		// voterdata << id << "\t";
+		// cout << "Please enter your name: "; cin >> name;
+		// cin.getline(name, 100);
+		// voterdata << name << "\t";
+		// cout << "Please enter your age:"; cin >> age;
+		// cin.getline(age, 100); cin >> age;
+		// voterdata << age << "\t";
+		// cin.ignore();
 		
 	}
-	else if (menu == s) {
+	else if (menu == "s" || menu == "S") {
 		// write code here 
+		cout << "testing option S";
 	}
-	else if (menu == l) {
+	else if (menu == "l" || menu == "L") {
 		// write code here 
+		cout << "testing option L";
 	}
-	else if (menu == q) {
-		cout << "Quit";
+	else if (menu == "q" || menu == "Q") {
+		cout << "Quit\n" << endl; 
 	}
 	else {
-		cout << "Unknown selection, please try again."
+		cout << "Unknown selection, please try again.";
+		// Calling main function to go back to list of selections 
+		main();
 	}
 	
 return 0;
@@ -131,4 +134,3 @@ return 0;
 //			candidatedata.close();
 //		}
 //		else cout << "Unable to open file";
->>>>>>> 0b11ac42de7c4b85f9c4dceb7522cb05a85dd21c
