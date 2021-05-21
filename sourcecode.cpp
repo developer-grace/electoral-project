04b91295d94ef22eae91ddd761c1bd936a57c3f7
LAPTOP-ERSNR4C4\Lorien <lorienc.fhs@gmail.com>
2021-05-21 12:37:38 +1000

    using arrays

--- sourcecode.cpp
+++ sourcecode.cpp
@@ -8,25 +8,24 @@ using namespace std;
 
 int main()
 {
-	int vote, identifier;
-	//vote = *count;
+	int length, vote, identifier;
 	string id;
-	// 
+	length = id.length();
 	
 	//rows, collumns
 	string candidates[15][6] = 
 	{ 
 		{"Name\t\t", "Identifier\t", "Party\t\t", "Age\t", "Electorate\t", "Vote"}, 
-		{"Jenny Goodie\t", "1\t\t", "Greens\t\t","36\t","Melbourne\t", ""},
-		{"Harry Badgoi\t", "2\t\t", "Liberal\t", "53\t", "Brighton\t", ""},
-		{"Robert Miner\t", "3\t\t",  "Liberal\t", "61\t", "Carrum\t", ""},
-		{"Samson Forresta\t", "4\t\t", "Greens\t\t", "39\t", "Hawthorn\t", ""},
-		{"Elena Werker\t", "5\t\t", "Labour\t\t", "42\t", "Preston\t", ""},
-		{"William Iamme\t", "6\t\t", "Independent\t", "29\t", "Essendon\t", ""},
-		{"Isabelle \t", "7\t\t", "Labour\t\t", "47\t", "Ivanhoe\t", ""},
-		{"David Doitte\t", "8\t\t", "Labour\t\t", "55\t", "", ""},
-		{"Michael McGhie\t", "9\t\t", "Independent\t", "32\t", "", ""},
-		{"Tara Bell\t", "10\t\t", "Liberal\t", "65\t", "", ""}
+		{"Jenny Goodie\t", "1\t\t", "Greens\t\t","36\t","Melbourne\t", "3"},
+		{"Harry Badgoi\t", "2\t\t", "Liberal\t", "53\t", "Brighton\t", "17"},
+		{"Robert Miner\t", "3\t\t",  "Liberal\t", "61\t", "Carrum\t\t", "8"},
+		{"Samson Forresta\t", "4\t\t", "Greens\t\t", "39\t", "Hawthorn\t", "10"},
+		{"Elena Werker\t", "5\t\t", "Labour\t\t", "42\t", "Preston\t", "55"},
+		{"William Iamme\t", "6\t\t", "Independent\t", "29\t", "Essendon\t", "5"},
+		{"Isabelle \t", "7\t\t", "Labour\t\t", "47\t", "Ivanhoe\t", "43"},
+		{"David Doitte\t", "8\t\t", "Labour\t\t", "55\t", "", "72"},
+		{"Michael McGhie\t", "9\t\t", "Independent\t", "32\t", "", "11"},
+		{"Tara Bell\t", "10\t\t", "Liberal\t", "65\t", "", "87"}
 	};
 	
 	string voters[15][5] = 
@@ -55,7 +54,7 @@ int main()
 	{
 		for (int i = 0; i < 15; i++)
 			{
-				for (int j = 0; j < 7; j++)
+				for (int j = 0; j < 6; j++)
 				{
 					cout << candidates[i][j] << " ";
 				}
@@ -68,7 +67,7 @@ int main()
 	{
 		cout << "Please enter a your 9 integer voting ID:\n";
 		cin >> id;
-		int length = id.length();
+		
 		if (length == 9)
 		{
 			cout << "Please enter your votes for the following candidates\n";
@@ -85,7 +84,10 @@ int main()
 			switch (identifier)
 			{
 			case 1:
-				cout << "test success!";
+				cout << "Enter you vote for Jenny Goodie\n";
+				cin >> vote;
+
+				//cout << "test success!";
 				break;
 			case 2:
 				break;
